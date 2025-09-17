7a830160b1296a04cc45717da54c338d50839ec0
OH DO HYEOK <dhehgur0125@gmail.com>
2025-09-17 17:25:04 +0900

    lab3.main

--- lab3/source/main.cpp
+++ lab3/source/main.cpp
@@ -6,31 +6,36 @@
 #include<glm/gtx/string_cast.hpp>
 #include <glm/gtc/constants.hpp>
 
-//const double DEGREES_TO_RADIANS = glm::pi<float>() / 180.0;
-//const double RADIANS_TO_DEGREES = 180.0 / glm::pi<float>();
 
 int main()
 {
-	/*glm::vec3 v(2, -1, 1);
-	glm::vec3 u(1, 1, 2);
-	float d = glm::dot(v, u);
-	float c = d / (glm::length(u) * glm::length(v));
-	float deg = glm::acos(c);
-	float deg2 = glm::degrees(deg);
-	std::cout << deg2 << std::endl;*/
+	/*glm::vec3 u(1, 0, 0);
+	glm::vec3 v(-1, 1, 0);
+	glm::vec3 c = glm::cross(u, v);
+	float i = glm::length(c);
+	float ang = glm::acos(glm::dot(u, v) / (glm::length(u) * glm::length(v)));
+	float i2 = glm::length(u) * glm::length(v) * sin(ang);
 
-	/*glm::vec3 v(1, 2, -1);
-	glm::vec3 u(0, 1, 0);
-	glm::vec3 b = glm::dot(v, u) * u;
-	glm::vec3 a = v - b;
-	std::cout << "a: " << glm::to_string(a) << std::endl;
-	std::cout << "b: " << glm::to_string(b) << std::endl;*/
+	glm::vec3 u2(1, 0, 0);
+	glm::vec3 v2(0, 1, 0);
+	float area = glm::length(glm::cross(u2, v2));
 
-	glm::vec3 n(0, 1, 0);
-	glm::vec3 p(1, 0, 1);
-	glm::vec3 x(-3, 3, 2);
-	glm::vec3 xp = x - p;
-	float di = glm::dot(xp, n);
-	std::cout << "°Å¸® : " << di << std::endl;
+	glm::vec3 u3(1, 0, 0);
+	glm::vec3 v3(1, 0, 0);
+	float area = glm::length(glm::cross(u3, v3));
+
+	glm::vec3 p1(1, 0, 0);
+	glm::vec3 p2(1, 1, 0);
+	glm::vec3 p3(-1, 0, 0);
+	glm::vec3 v1 = p2 - p1;
+	glm::vec3 v2 = p3 - p1;
+	float area2 = 0.5f * glm::length(glm::cross(v1, v2));*/
+
+	glm::vec3 p1(3.0f, 0.0f, 0.0f);
+	glm::vec3 p2(1.5f, 0.86f, 0.0f);
+	glm::vec3 p3(3.0f, 0.0f, -1.0f);
+	glm::vec3 u = glm::normalize(glm::cross(p2 - p1, p3 - p1));
+	std::cout << glm::to_string(u) << std::endl;
+
 	return 0;
 }
